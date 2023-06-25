e54dc37a904891ed21994a2a4b73b2a0514dad05
flappyzoo123456 <137706541+flappyzoo123456@users.noreply.github.com>
2023-06-25 16:29:42 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Diese Seite bei [https://flappyzoo123456.github.io/julia-helene/](https://flappyzoo123456.github.io/julia-helene/) öffnen
+
+## Als Erweiterung verwenden
+
+Dieses Repository kann als **Erweiterung** in MakeCode hinzugefügt werden.
+
+* öffne [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* klicke auf **Neues Projekt**
+* klicke auf **Erweiterungen** unter dem Zahnrad-Menü
+* nach **https://github.com/flappyzoo123456/julia-helene** suchen und importieren
+
+## Dieses Projekt bearbeiten ![Build status badge](https://github.com/flappyzoo123456/julia-helene/workflows/MakeCode/badge.svg)
+
+Um dieses Repository in MakeCode zu bearbeiten.
+
+* öffne [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* klicke auf **Importieren** und dann auf **Importiere URL**
+* füge **https://github.com/flappyzoo123456/julia-helene** ein und klicke auf Importieren
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/flappyzoo123456/julia-helene/raw/master/.github/makecode/blocks.png)
+
+#### Metadaten (verwendet für Suche, Rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Automatisch generierter Code. Nicht bearbeiten.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Level1":
+            case "Level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Automatisch generierter Code. Nicht bearbeiten.
